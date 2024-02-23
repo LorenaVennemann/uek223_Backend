9f90c1e38d79b88b921b3f63ba2543ab8eabadf4
burak-adanir <oemer.adanir@noseryoung.com>
2024-02-23 14:16:34 +0100

    fix error in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,22 +12,27 @@ VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', 'DEFAULT'),
 ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', 'USER')
 ON CONFLICT DO NOTHING;
 
---AUTHORITIES
 INSERT INTO authority(id, name)
-VALUES ('2ebf301e-6c61-4076-98e3-2a38b31daf86', 'DEFAULT'),
-('76d2cbf6-5845-470e-ad5f-2edb9e09a868', 'USER_MODIFY'),
-('21c942db-a275-43f8-bdd6-d048c21bf5ab', 'USER_DELETE'),
-('1e29a363-d231-4a40-9ee9-b194909a3cb2','POST_CREATE')
-ON CONFLICT DO NOTHING;
+VALUES
+    ('2ebf301e-6c61-4076-98e3-2a38b31daf86', 'DEFAULT'),
+    ('76d2cbf6-5845-470e-ad5f-2edb9e09a868', 'USER_MODIFY'),
+    ('21c942db-a275-43f8-bdd6-d048c21bf5ab', 'USER_DELETE'),
+    ('1e29a363-d231-4a40-9ee9-b194909a3cb2', 'POST_CREATE')
+    ON CONFLICT DO NOTHING;
+
+
+-- Assign POST_CREATE authority to USER role
+INSERT INTO role_authority(role_id, authority_id)
+VALUES ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '1e29a363-d231-4a40-9ee9-b194909a3cb2')
+    ON CONFLICT DO NOTHING;
+
 
 --assign roles to users
 insert into users_role (users_id, role_id)
 values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1'),
        ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1'),
        ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'ab505c92-7280-49fd-a7de-258e618df074'),
-       ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
-       ('ab505c92-7280-49fd-a7de-258e618df074','1e29a363-d231-4a40-9ee9-b194909a3cb2'),
-       ('c6aee32d-8c35-4481-8b3e-a876a39b0c02','1e29a363-d231-4a40-9ee9-b194909a3cb2 ')
+       ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02')
  ON CONFLICT DO NOTHING;
 
 --assign authorities to roles
